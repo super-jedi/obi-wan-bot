16c4b3669e6976a2bd17ef045a22b2f5f15b6c21
Harikrishnanps <harikrishnan.ps@hotmail.com>
2020-04-21 02:52:34 +0530

    error fix

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ client.on("message", msg => {
     }
 
     else if(msg.content === "!help"){
-        msg.reply("The jedi council only allows me to respond to two things: !hello and !help and !thank the maker")
+        msg.reply("The jedi council only allows me to respond to three things: !hello and !help and !thank the maker")
     }
     
     else if(msg.content === "!thank the maker"){
