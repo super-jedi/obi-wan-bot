882bda637d720b3d12c6699041c219b8f72e5c68
Harikrishnanps <harikrishnan.ps@hotmail.com>
2020-04-21 01:38:12 +0530

    initial files

---
+++ app.js
@@ -0,0 +1,31 @@
+//require discord js
+const Discord = require("discord.js");
+
+//create new client
+const client = new Discord.Client();
+
+
+// Display a message once the bot has started
+client.on("ready", ()=> {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+// Check messages for a specific command
+client.on("message", msg => {
+    // Send back a reply when the specific command has been written
+    if (msg.content === "!hello") {
+        msg.reply("WELL HELLO THERE !");
+    }
+
+    else if(msg.content === "!help"){
+        msg.reply("The jedi council only allows me to respond to two things: !hello and !help")
+    }
+});
+
+
+
+
+
+//log in with token
+client.login("NzAxODM5NTk3NTQxNTIzNTc3.Xp3V5g.uVXQ4O_xxenUAqGHuBwsJ8-nuFs");
