5ccf99cd5e1dd591581dc52a7d1b643fd27f31fc
Harikrishnanps <harikrishnan.ps@hotmail.com>
2020-04-23 15:41:36 +0530

    mm

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const client = new Discord.Client();
 // Display a message once the bot has started
 client.on("ready", ()=> {
     console.log(`Logged in as ${client.user.tag}!`);
-    msg.reply("WHAT ABOUT THE DROID ATTACK ONN THE WOOKIES?");
+    msg.channel.send("WHAT ABOUT THE DROID ATTACK ONN THE WOOKIES?");
 });
 
 
@@ -17,19 +17,19 @@ client.on("ready", ()=> {
 client.on("message", msg => {
     // Send back a reply when the specific command has been written
     if (msg.content === "hello") {
-        msg.reply("WELL HELLO THERE !");
+        msg.channel.send("WELL HELLO THERE !");
     }
 
     else if(msg.content === "help"){
-        msg.reply("The jedi council only allows me to respond to these things: hello and help and thank the maker and who are you?")
+        msg.channel.send("The jedi council only allows me to respond to these things: hello and help and thank the maker and who are you?")
     }
     
     else if(msg.content === "thank the maker"){
-        msg.reply("I WAS CREATED BY THE GREAT HARIKRISHNAN PS. HE IS AWESOME. ALL THANKS TO HIM")
+        msg.channel.send("I WAS CREATED BY THE GREAT HARIKRISHNAN PS. HE IS AWESOME. ALL THANKS TO HIM")
     }
     
     else if(msg.content === "who are you?"){
-        msg.reply("I AM MASTER OBI WAN KENOBI OF THE JEDI HIGH COUNCIL")
+        msg.channel.send("I AM MASTER OBI WAN KENOBI OF THE JEDI HIGH COUNCIL")
     }
 
 });
