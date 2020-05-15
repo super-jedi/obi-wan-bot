7faa19ad67049a29f250ae1a0993f5b8df3cfcc3
Harikrishnanps <harikrishnan.ps@hotmail.com>
2020-05-16 02:37:48 +0530

    heisting with dankmemer

--- app.js
+++ app.js
@@ -32,6 +32,18 @@ client.on("message", msg => {
         msg.channel.send("I AM MASTER OBI WAN KENOBI OF THE JEDI HIGH COUNCIL")
     }
 
+    else if(msg.content === "republic credits will do just fine"){
+        msg.channel.send("Pls daily")
+    }
+    else if(msg.content === "republic credits will do just fine"){
+        msg.channel.send("t!daily")
+    }
+    else if(msg.content === "droid attack on the wookies"){
+        msg.channel.send("Pls steal @thevishnu")
+    }
+
+    
+    
 });
 
 
